c1af22bd302a8dca566e0d3d38e3016c48da7383
woodencatty <woodencatty@gmail.com>
2018-10-11 12:55:13 +0900

    r4u?

--- Asung/AIUse.js
+++ Asung/AIUse.js
@@ -3,15 +3,16 @@ var fs = require("fs");
 
 var net = new brain.NeuralNetwork();
 
-function loadFile() {
-    var obj = JSON.parse(fs.readFileSync('network.json', 'utf8'));
+module.exports = {
+    loadFile: () => {
+        var obj = JSON.parse(fs.readFileSync('network.json', 'utf8'));
     net.fromJSON(obj);
     console.log("file loaded");
+    },
+
+    run4analyse: (callback) => {
+        var output = net.run({input: { Noise: -13, Temp: 97, Humi: -459, Lux: -13, Pose: 97 }});   // Walking Data
+
+                callback(output);
+    }
 }
- 
-loadFile(); 
-
-
-var output = net.run({input: { Noise: -13, Temp: 97, Humi: -459, Lux: -13, Pose: 97 }});   // Walking Data
-
-console.log(output);
--- Nayeon/AIUse.js
+++ Nayeon/AIUse.js
@@ -3,15 +3,16 @@ var fs = require("fs");
 
 var net = new brain.NeuralNetwork();
 
-function loadFile() {
-    var obj = JSON.parse(fs.readFileSync('network.json', 'utf8'));
+module.exports = {
+    loadFile: () => {
+        var obj = JSON.parse(fs.readFileSync('network.json', 'utf8'));
     net.fromJSON(obj);
     console.log("file loaded");
+    },
+
+    run4analyse: (callback) => {
+        var output = net.run({ material : 1, moist_start : 0.3, drytime : 60, try_temp : 80, air : 30});   // Data
+
+                callback(output);
+    }
 }
- 
-loadFile(); 
-
-
-var output = net.run({ material : 1, moist_start : 0.3, drytime : 60, try_temp : 80, air : 30});   // Data
-
-console.log(output);
