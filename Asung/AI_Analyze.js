2f43e7de0833e08a3a32a4235706e66d9f670ecf
woodencatty <woodencatty@gmail.com>
2018-11-07 15:33:03 +0900

    well

--- Asung/AI_Analyze.js
+++ Asung/AI_Analyze.js
@@ -1,18 +1,61 @@
+var request = require('request');
 
 
 //예측해서 경광등으로 알려주자!
 //그럼 게이트웨이로 보내야하는데 게이트웨이 정보를 알아야하네?
 //답이없네 이거
 
+var Train_set = [];
+var Test_set = [];
 
-function sometime_train(){
+var Train_net = new brain.NeuralNetwork({
+    hiddenLayers: [5, 3],
+    activation: 'sigmoid'
+});
 
-setTimeout(() => {
-    
-}, 24*7*60*60*60);
+var Test_net = new brain.NeuralNetwork();
+
+
+
+function sometime_train() {
+
+request('http://gachon.ac.kr', function (error, response, body) {
+    if(response.statusCode != 200){
+        console.log('error:', error); // Print the error if one occurred
+    }else{
+        for(var i in body){
+            Train_set.push({ input: { TEMPERATURE: rows[i].TEMPERATURE, HUMIDITY: rows[i].HUMIDITY, LIGHT: rows[i].LIGHT, SOUND: rows[i].SOUND, POSE: rows[i].POSE }, output: { Status: OK } })
+        }
+
+
+    }
+});
+}
+
+function check_use(deviceID) {
 
 }
 
-function check_use(){
-    
+function makeDeviceInterval(device_ID) {
+
+    check_use(device_ID)
+
 }
+
+sometime_train();
+
+/*
+setInterval(() => {
+
+
+
+    sometime_train();
+
+    if ("new device added" == 1) {
+        makeDeviceInterval(deviceID);
+    }
+
+}, 1000 * 60 * 60 * 24);
+
+
+*/
--- Nayeon/tst.js
+++ Nayeon/tst.js
@@ -1,6 +1,18 @@
-
+var request = require('request');
 
 var jsn = { input: { material : 1, moist_start : 0.3, drytime : 60, try_temp : 80, air : 30}, output: { TargetReached : 1 } };
 
 console.log(jsn);
 
+
+
+request
+  .get('http://gachon.ac.kr/')
+  .on('response', function(response) {
+    console.log(response.statusCode) // 200
+    console.log(response.headers['content-type'])
+    console.log(response.url); // 'image/png'
+  })
+  .on('data', function(data){
+      console.log(data.toString());
+  })
