f1e0a90d38ffce14962a0b0b1f05dd5dd3260577
woodencatty <woodencatty@gmail.com>
2018-11-07 15:48:55 +0900

    gazua

--- Asung/AI_Analyze.js
+++ Asung/AI_Analyze.js
@@ -19,26 +19,48 @@ var Test_net = new brain.NeuralNetwork();
 
 function sometime_train() {
 
-request('http://gachon.ac.kr', function (error, response, body) {
-    if(response.statusCode != 200){
-        console.log('error:', error); // Print the error if one occurred
-    }else{
-        for(var i in body){
-            Train_set.push({ input: { TEMPERATURE: rows[i].TEMPERATURE, HUMIDITY: rows[i].HUMIDITY, LIGHT: rows[i].LIGHT, SOUND: rows[i].SOUND, POSE: rows[i].POSE }, output: { Status: OK } })
+    request('127.0.0.1:8080/ai/data/all', function (error, response, body) {
+        if (response.statusCode != 200) {
+            console.log('error:', error); // Print the error if one occurred
+        } else {
+            for (var i in body) {
+                Train_set.push({ input: { meterial: drydata_split.material, moist_start: drydata_split.moist_start, dry_time: drydata_split.dry_time, dry_temp: drydata_split.dry_temp, air: drydata_split.air }, output: { isOK: isOK } })
+            }
+            Train_net.train(trainSet);
+
+            fs.writeFile("network.json", JSON.stringify(Train_net.toJSON()), function (err) {
+                if (err)
+                    return console.log(err);
+    
+                console.log("The train file was saved");
+            });
+    
         }
-
-
-    }
-});
+    });
 }
 
 function check_use(deviceID) {
+
+
+    request('127.0.0.1:8080/ai/data/'+deviceID, function (error, response, body) {
+        if (response.statusCode != 200) {
+            console.log('error:', error); // Print the error if one occurred
+        } else {
+
+            
+        }
+    });
 
 }
 
 function makeDeviceInterval(device_ID) {
 
+setInterval(() => {
     check_use(device_ID)
+
+},  1000 * 60 * 60);
+
+
 
 }
 
